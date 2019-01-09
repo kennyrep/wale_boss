cb960bfd4c76b41a871e0c933000dec1d91c3e50
Ahmed Olawale <ahmedmolawale@gmail.com>
2019-01-09 12:41:14 +0100

    Review 1 - Added Todo items

--- app/src/main/java/com/example/kennyrep/stanbic/Ajayi_Ventures.java
+++ app/src/main/java/com/example/kennyrep/stanbic/Ajayi_Ventures.java
@@ -3,6 +3,9 @@ package com.example.kennyrep.stanbic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//TODO 10. naming of classes in JAVA follows a convention called CamelCasing - capitalize the first letter of each word.
+// TODO This should be AjayiVentures and not Ajayi_Ventures. This same principle should apply to other Classes.
 public class Ajayi_Ventures extends AppCompatActivity {
 
     @Override
@@ -11,3 +14,6 @@ public class Ajayi_Ventures extends AppCompatActivity {
         setContentView(R.layout.activity_ajayi__ventures);
     }
 }
+
+//Todo 11: Look at the drawable folder, the back.png folder has the corresponding resolution formats. Then what happens to bridge.png
+// Todo I know the design folder i gave you has all the images in their different format - hdpi, mdpi, and so on. Do the needful to all the images
